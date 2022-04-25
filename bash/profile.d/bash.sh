abca4bc170d187e4ea4bc3f96d5faa9723e07945
Craig Sloggett <craigsloggett@gmail.com>
2022-04-25 10:37:07 -0400

    bash: Initial commit

---
+++ bash/profile.d/bash.sh
@@ -0,0 +1,5 @@
+#
+# ${XDG_CONFIG_HOME}/bash/profile.d/bash.sh
+#
+
+export HISTFILE="${XDG_STATE_HOME}/bash/history"
---
+++ bash/profile.d/gpg.sh
@@ -0,0 +1,6 @@
+#
+# ${XDG_CONFIG_HOME}/bash/profile.d/gpg.sh
+#
+
+export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
+export GPG_TTY="$(tty)"
