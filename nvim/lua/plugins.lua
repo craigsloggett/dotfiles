ae2a6fe88d13a5f6b4bbfdf3b601792bbfb7015b
Craig Sloggett <craigsloggett@gmail.com>
2024-01-26 15:01:45 -0500

    nvim: Add plugins and language server support

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,40 @@
+-- Setup the lazy.nvim package manager.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Specify which plugins to install.
+require("lazy").setup({
+  {
+    -- Highlight, edit, and navigate code.
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate"
+  },
+  {
+    -- Manage language servers.
+    "williamboman/mason.nvim",
+    config = true
+  },
+  {
+    -- Manage language servers configuration.
+    "williamboman/mason-lspconfig.nvim"
+  }
+})
+
+-- Include the configuration for each plugin.
+require("plugins/treesitter")
+require("plugins/mason-lspconfig")
