3f68a6a27332a631ffc729d4df08da7c8b0d1c20
Craig Sloggett <craigsloggett@gmail.com>
2024-02-02 14:06:25 -0500

    nvim: Set treesitter to autoinstall languages

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "bash" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
