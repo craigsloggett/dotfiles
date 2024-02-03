99aec4ddd60c985999f3a8a48d7ec6e983c51224
Craig Sloggett <craigsloggett@gmail.com>
2024-02-03 09:53:37 -0500

    nvim: Add lazy.nvim and treesitter configuration

--- nvim/lua/options.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
--- nvim/lua/plugins/lspconfig.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    require("language-servers/lua_ls")
-  end,
-}
