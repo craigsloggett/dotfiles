0bb78fe4e006d32645259498acc84a21ef4dccd6
Craig Sloggett <craigsloggett@gmail.com>
2024-02-03 21:13:04 -0500

    nvim: Add keymap config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 -- Configure vim options before loading any plugins.
 require("config.options")
+require("config.keymaps")
 -- Bootstrap the lazy.nvim plugin manager.
 require("config.lazy")
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+-- Common options to all keymaps.
+local opts = {
+  noremap = true,
+  silent = true,
+}
+
+-- Better window navigation.
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+-- Resize with arrows.
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+-- Keep lines highlighted after an indentation change.
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
