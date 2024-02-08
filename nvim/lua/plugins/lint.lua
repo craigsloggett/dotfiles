8022bd455111c4012d9d5a644b4b2bd872a4dea4
Craig Sloggett <craigsloggett@gmail.com>
2024-02-08 07:07:28 -0500

    nvim: Lint on text change for quicker feedback

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -16,6 +16,7 @@ return {
         "BufEnter",
         "BufWritePost",
         "InsertLeave",
+        "TextChanged",
       }, {
         callback = function()
           require("lint").try_lint()
