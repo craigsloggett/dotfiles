b362d8e60fd5887686dc8c6af4ab2217eb51d59a
Craig Sloggett <craigsloggett@gmail.com>
2024-02-12 07:47:17 -0500

    nvim: Add XDG support for Python

---
+++ zsh/.zprofile.d/python.zsh
@@ -0,0 +1,7 @@
+#
+# $ZDOTDIR/.zshenv.d/python.zsh
+#
+
+export PYTHON_HISTORY="${XDG_STATE_HOME}/python/history"
+export PYTHONPYCACHEPREFIX="${XDG_CACHE_HOME}/python"
+export PYTHONUSERBASE="${XDG_DATA_HOME}/python"
