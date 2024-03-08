b78bc8a35c5add38e11362ec6d8513c4c86353de
Craig Sloggett <craigsloggett@gmail.com>
2024-03-08 12:26:39 -0500

    nvim: Customize indentation for specific filetypes

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,9 +2,9 @@ vim.g.mapleader = " "
 
 vim.cmd("colorscheme habamax")
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
 vim.opt.number = true
@@ -17,3 +17,15 @@ vim.opt.incsearch = true
 vim.opt.hlsearch = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Autocommand to set options for Python filetype
+vim.api.nvim_create_augroup("PythonSettings", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "PythonSettings",
+  pattern = "python",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
