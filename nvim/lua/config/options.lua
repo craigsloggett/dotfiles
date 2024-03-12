06d2dcdc7d5b93522e580ac8605e1d9b14324a6a
Craig Sloggett <craigsloggett@gmail.com>
2024-03-12 13:16:30 -0400

    nvim: Add go settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -31,3 +31,16 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 4
   end,
 })
+
+-- Autocommand to set options for Go filetype
+vim.api.nvim_create_augroup("GoSettings", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "GoSettings",
+  pattern = "go",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt.expandtab = false
+  end,
+})
