08775c2fd56e12ff30944bea79e963faff5a089f
Craig Sloggett <craigsloggett@gmail.com>
2026-03-06 14:10:58 -0500

    claude: Remove symlinks to XDG directories

--- zsh/.zprofile.d/claude.zsh
+++
@@ -1,9 +0,0 @@
-#
-# $ZDOTDIR/.zprofile.d/claude.zsh
-#
-
-# Claude Code does not respect XDG Base Directory Specification.
-# https://github.com/anthropics/claude-code/issues/1455
-#
-# Subdirectories inside ~/.claude are symlinked to XDG locations
-# by the install script. See install:_claude() for details.
