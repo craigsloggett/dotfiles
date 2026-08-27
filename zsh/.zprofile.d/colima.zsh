f9d58acf9492c763778a4609b98810a750043636
Craig Sloggett <craigsloggett@gmail.com>
2026-08-27 13:38:47 -0400

    docker: Add initial configuration

---
+++ zsh/.zprofile.d/colima.zsh
@@ -0,0 +1,6 @@
+#
+# $ZDOTDIR/.zprofile.d/colima.zsh
+#
+
+export COLIMA_HOME="${XDG_CONFIG_HOME}/colima"
+export LIMA_HOME="${XDG_DATA_HOME}/lima"
---
+++ zsh/.zprofile.d/docker.zsh
@@ -0,0 +1,6 @@
+#
+# $ZDOTDIR/.zprofile.d/docker.zsh
+#
+
+export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
+export BUILDX_CONFIG="${XDG_STATE_HOME}/docker/buildx"
