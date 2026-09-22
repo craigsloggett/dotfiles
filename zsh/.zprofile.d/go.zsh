273a5b429a53a14e9d0f7896b1adb089f9e26103
Craig Sloggett <craigsloggett@gmail.com>
2026-09-22 09:35:26 -0400

    go: Add GOBIN to environment

--- zsh/.zprofile.d/go.zsh
+++ zsh/.zprofile.d/go.zsh
@@ -3,6 +3,7 @@
 #
 
 export GOPATH="${XDG_DATA_HOME}/go"
+export GOBIN="${XDG_BIN_HOME}"
 export GOMODCACHE="${XDG_CACHE_HOME}/go/mod"
 export GOCACHE="${XDG_CACHE_HOME}/go/build"
 export GOENV="${XDG_CONFIG_HOME}/go/env"
